48bd803af5958d64de9016050b1e340367490f03
Seth Bird <seth.a.bird@gmail.com>
2012-10-23 19:00:23 -0400

    Started working on Conversion so that this works with Sublime 2

--- chooseAnyCommandPlugin.py
+++ chooseAnyCommandPlugin.py
@@ -17,19 +17,17 @@ class ChooseAnyCommandCommand(sublimeplugin.WindowCommand):
     to_sort = []
 
     for cmd_type, runner in cmd_runners(window).items():
-      cmds_of_type = getattr(sublimeplugin, '%sCommands' % cmd_type)
-      is_enabled = getattr(runner, 'canRunCommand', None)
-      if not is_enabled: continue
+      cmds_of_type = getattr(sublimeplugin, '%s_command_classes' % cmd_type)
       for cmd in cmds_of_type:
-        if is_enabled(cmd):
-          to_sort.append((cmd_type, cmds_of_type[cmd].__module__, cmd))
+        to_sort.append((cmd_type, cmd.__module__, cmd))
 
     to_sort.sort()
 
-    window.showQuickPanel("", "executeNamedCommand", 
-        [`(c[0], c[2])` for c in to_sort],
-        ['\\'.join(c) for c in to_sort],
-        sublime.QUICK_PANEL_FILES )
+    window.show_quick_panel(to_sort, 
+    #window.show_quick_panel("", "executeNamedCommand", 
+        #[`(c[0], c[2])` for c in to_sort],
+        #['\\'.join(c) for c in to_sort],
+        #sublime.QUICK_PANEL_FILES )
 
 class ExecuteNamedCommandCommand(sublimeplugin.WindowCommand):
   def run(self, window, args):
